b62d888200304ef745e2d6bbd431934e83cfb398
JaesungLeee <viki0114@naver.com>
2023-03-02 22:11:54 +0900

    [BasicCodelabs] 4. Tweakint the UI

--- codelabs/jetpack-compose-for-android-developers/pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ codelabs/jetpack-compose-for-android-developers/pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -3,13 +3,18 @@ package com.codelab.basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.codelab.basics.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +36,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
+}
+
+/**
+ * Surface
+ * background color에 따라 contentColor가 내부적으로 Material ColorSet에 따라 달라짐
+ * @see Colors.contentColorFor
+ */
+@Composable
+fun CircularGreeting(name: String) {
+    Surface(
+        modifier = Modifier.padding(8.dp),
+        border = BorderStroke(2.dp, Color.Red),
+        contentColor = Color.Blue,
+        elevation = 8.dp,
+        shape = CircleShape,
+        color = Color.Green,
+
+    ) {
+        Text(text = "Hello $name", modifier = Modifier.padding(8.dp))
+    }
 }
 
 @Preview(showBackground = true)
@@ -39,5 +66,13 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     BasicsCodelabTheme {
         Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true, name = "Round Surface")
+@Composable
+fun CircularGreetingPreview() {
+    BasicsCodelabTheme {
+        CircularGreeting(name = "Android")
     }
 }
