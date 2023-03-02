36f448d497c915f2f7eafe543aa79816b79246b0
JaesungLeee <viki0114@naver.com>
2023-03-03 00:36:18 +0900

    [BasicCodelabs] 7. State in Compose

--- codelabs/jetpack-compose-for-android-developers/pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ codelabs/jetpack-compose-for-android-developers/pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BasicsCodelabTheme {
-                MyApp(modifier = Modifier.fillMaxSize())
+                ColumnMyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
@@ -72,26 +72,44 @@ fun CircularGreeting(name: String) {
         contentColor = Color.Blue,
         elevation = 8.dp,
         shape = CircleShape,
-        color = Color.Green,
-
-        ) {
+        color = Color.Green
+    ) {
         Text(text = "Hello $name", modifier = Modifier.padding(8.dp))
     }
 }
 
+/**
+ * 로컬 변수로 expanded를 선언하면 이벤트에 의한 리컴포지션 발생 시 컴포즈에서 tracking하지 못함 (Side Effect)
+ * mutableStateOf API는 Compose가 리컴포지션 시 State를 저장할 수 있도록 함
+ * remember와 함께 사용해야 Recomposition이 발생해도 State가 재설정되지 않고 유지할 수 있게 됨
+ */
 @Composable
 fun ColumnGreeting(name: String) {
+
+//    val expanded = remember { mutableStateOf(false) } // should use with .value
+
+    var expanded by remember { mutableStateOf(false) }
+    val extraPadding = if (expanded) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
-            OutlinedButton(onClick = { /*TODO*/ }) {
-                Text(text = "Show more")
+            OutlinedButton(
+                onClick = {
+                    expanded = !expanded
+                }
+            ) {
+                Text(text = if (expanded) "Show less" else "Show more")
             }
         }
     }
