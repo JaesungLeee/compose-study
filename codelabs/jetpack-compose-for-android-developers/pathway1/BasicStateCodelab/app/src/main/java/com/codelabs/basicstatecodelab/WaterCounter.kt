293b84d548b1d0e06c75e20f675469a5061755f6
JaesungLeee <viki0114@naver.com>
2023-03-04 17:40:17 +0900

    [BasicStateCodelab] Step4. Events in Compose

--- codelabs/jetpack-compose-for-android-developers/pathway1/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ codelabs/jetpack-compose-for-android-developers/pathway1/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -1,6 +1,9 @@
 package com.codelabs.basicstatecodelab
 
+import android.util.Log
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,9 +17,16 @@ import androidx.compose.ui.unit.dp
  */
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0  // 잘못된 State 선언
-    Text(
-        text = "You've had $count glasses.",
-        modifier = modifier.padding(16.dp)
-    )
+    Column(modifier = modifier.padding(16.dp)) {
+        var count = 0  // 잘못된 State 선언
+        Text(
+            text = "You've had $count glasses."
+        )
+        Button(
+            onClick = { count++ },
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Text(text = "Add One")
+        }
+    }
 }
